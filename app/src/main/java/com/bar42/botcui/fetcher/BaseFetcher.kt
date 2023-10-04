87d0ff989caf62a5f8ec5d5f27159468141d1bf4
dpikulkin <dpikulkin@openwaygroup.com>
2023-10-04 11:27:37 +0300

    New URL for backend

--- app/src/main/java/com/bar42/botcui/fetcher/BaseFetcher.kt
+++ app/src/main/java/com/bar42/botcui/fetcher/BaseFetcher.kt
@@ -4,10 +4,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
 
-open class BaseFetcher() {
+open class BaseFetcher {
     companion object {
         private val retrofit = Retrofit.Builder()
-            .baseUrl(URL("https://df1c-62-228-134-33.ngrok-free.app/api/v1/"))
+            .baseUrl(URL("https://sound-mink-natural.ngrok-free.app/api/v1/"))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val gameInterface = retrofit.create(GameInterface::class.java)
